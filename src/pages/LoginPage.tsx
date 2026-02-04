63a4c6285c105c8bfa7de65a248d7ab0145c980a
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-02-04 21:22:27 +0100

    added login layout and management

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { useState } from "react";
+import { useAuthContext } from "../features/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+   const { login } = useAuthContext();
+   const navigate = useNavigate();
+
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [error, setError] = useState("");
+
+   const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+
+      try {
+         await login({ email, password });
+         navigate("/");
+      } catch (error) {
+         setError("Login fallito");
+      }
+   };
+
+   return (
+      <form
+         onSubmit={handleSubmit}
+         className="max-w-md mx-auto mt-20 flex flex-col gap-4 "
+      >
+         <input
+            className="border-2 border-gray-300 p-2 rounded-md"
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+         />
+         <input
+            className="border-2 border-gray-300 p-2 rounded-md"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+         />
+
+         {error && <p className="text-red-500">{error}</p>}
+
+         <button className="bg-blue-600 text-white p-2 rounded-md cursor-pointer mt-3">
+            Login
+         </button>
+      </form>
+   );
 };
