179c9cb935e4f7277c997ab2a819c56480ded024
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-02-04 21:31:43 +0100

    Installed React Query (TanStack Query) to manage cache, synchronization, and asynchronous query state (API) and makes the client available to all components within the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./index.css";
 import { AuthProvider } from "./features/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>,
+   <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+         <RouterProvider router={router} />
+      </AuthProvider>
+   </QueryClientProvider>,
 );
