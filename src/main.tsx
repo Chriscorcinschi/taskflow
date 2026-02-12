0e1046826fe1c15b6616497f26b127e2449f474e
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-02-12 16:54:38 +0100

    feat: wrapped app with chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { router } from "./router";
 import "./index.css";
 import { AuthProvider } from "./features/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "./components/ui/provider";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-   <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-         <RouterProvider router={router} />
-      </AuthProvider>
-   </QueryClientProvider>,
+   <Provider>
+      <QueryClientProvider client={queryClient}>
+         <AuthProvider>
+            <RouterProvider router={router} />
+         </AuthProvider>
+      </QueryClientProvider>
+   </Provider>,
 );
