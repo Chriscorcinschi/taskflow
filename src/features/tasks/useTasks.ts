f047ca2217fabb052baa4e6fd3e28c29a7b07f23
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-02-16 14:09:47 +0100

    feat: useTasks ts

---
+++ src/features/tasks/useTasks.ts
@@ -0,0 +1,50 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+   createTask,
+   deleteTask,
+   getTasks,
+   updateTaskStatus,
+} from "./tasks.api";
+import type { TaskStatus } from "../../types/task";
+
+export function useTasks() {
+   const queryClient = useQueryClient();
+
+   const tasksQuery = useQuery({
+      queryKey: ["tasks"],
+      queryFn: getTasks,
+   });
+
+   const createMutation = useMutation({
+      mutationFn: createTask,
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      },
+   });
+
+   const updateMutation = useMutation({
+      mutationFn: ({ id, status }: { id: string; status: TaskStatus }) =>
+         updateTaskStatus(id, status),
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      },
+   });
+
+   const deleteMutation = useMutation({
+      mutationFn: deleteTask,
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      },
+   });
+
+   return {
+      ...tasksQuery,
+      createTask: createMutation.mutateAsync,
+      updateTask: updateMutation.mutateAsync,
+      deleteTask: deleteMutation.mutateAsync,
+
+      isCreating: createMutation.isPending,
+      isUpdating: updateMutation.isPending,
+      isDeleting: deleteMutation.isPending,
+   };
+}
