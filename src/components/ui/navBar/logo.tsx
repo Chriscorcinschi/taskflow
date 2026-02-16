d955451390f49c397718518a6b7044f7975b10cf
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-02-16 14:05:22 +0100

    feat: logo

---
+++ src/components/ui/navBar/logo.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const Logo = () => {
+   return (
+      <Box pr={4}>
+         <Text fontSize="lg" fontWeight="bold" color="blue.600">
+            Task Flow
+         </Text>
+      </Box>
+   );
+};
